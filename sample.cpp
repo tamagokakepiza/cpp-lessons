b1024fcb8f0cfa22003f19c968be577c19bb3f28
tamagokakepiza <nyororo89@gmail.com>
2022-07-09 20:03:20 +0900

    セクション5-2

--- sample.cpp
+++ sample.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main (){
-  int x, y;
-  cout << "2つの数値を入力して下さい\n";
-  cin >> x >> y;
-  cout << "足し算の結果：" << x + y << "\n";
+  if(0)
+    cout << "処理が実行されます\n";
   return 0;
 }
