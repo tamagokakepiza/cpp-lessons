92594ccd52fc5008fd4a16d48fc948aab38f2478
tamagokakepiza <nyororo89@gmail.com>
2022-07-09 19:47:14 +0900

    セクション4-9

--- sample.cpp
+++ sample.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main (){
-  int x = 10;
-  int y = 10;
-  x++;
-  y--;
-  cout << "xの値:" << x << "\n";
-  cout << "yの値:" << y << "\n";
+  const int x = 10;
+  cout << x << "\n";
   return 0;
 }
