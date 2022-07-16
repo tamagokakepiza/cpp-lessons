359d01cf346d66d6bef725f3435eeb19d5bf4e60
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 12:21:32 +0900

    セクション6-4

--- sample.cpp
+++ sample.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main (){
-  int i;
-  for(i = 0; i < 5; i++){
-    cout << i+1 << "回目：メッセージ\n";
+  int i, j;
+  for(i = 1; i <= 9; i++){
+    cout << i << "の段：";
+    for(j = 1; j <= 9; j++){
+      cout << setw(5) << i * j;
+    }
+    cout << "\n";
   }
   return 0;
 }
