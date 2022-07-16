e8fa268827c2ca7cab6e2dcac5c38b33fdddfe8e
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 13:34:37 +0900

    セクション7-6

--- sample.cpp
+++ sample.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void twice(int x){
-  cout << x * 2 << "\n";
+void culc(int a, int b){
+  int sum;
+  sum = (300*a) + (200*b);
+  cout << "合計金額は" << sum << "円です\n";
 }
 
 int main (){
-  int num;
-  cout << "数値を入力して下さい\n";
-  cin >> num;
-  twice(num);
+  int apple, orange;
+  cout << "りんごの個数は？";
+  cin >> apple;
+  cout << "オレンジの個数は？";
+  cin >> orange;
+  culc(apple, orange);
   return 0;
 }
