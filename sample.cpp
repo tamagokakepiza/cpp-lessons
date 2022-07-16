c348a6e8e7d5b0eb4fb2364e2246a116a8729689
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 13:52:28 +0900

    セクション8-3

--- sample.cpp
+++ sample.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int culc(int a){
-  int sum;
-  sum = a * 200;
-  return sum;
-}
-
 int main (){
-  int apple;
-  cout << "りんごの個数は？";
-  cin >> apple;
-  int judge = culc(apple);
-  if(judge > 3000)
-    cout << "予算オーバーです\n";
-  else
-    cout << "購入できます";
+  int array[3];
+  array[0] = 10;
+  array[1] = 20;
+  array[2] = 30;
+  cout << array[0] << "\n";
+  cout << array[1] << "\n";
+  cout << array[2] << "\n";
   return 0;
 }
