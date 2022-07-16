a87c7aff92aef39ce193e3844fa2a085df2803a7
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 11:46:30 +0900

    セクション5-5

--- sample.cpp
+++ sample.cpp
@@ -5,11 +5,7 @@ int main (){
   int num;
   cout << "数値を入力して下さい";
   cin >> num;
-  if(num > 10)
-    cout << "10より大きいです\n";
-  if(num < 10)
-    cout << "10より小さいです\n";
-  if(num == 10)
-    cout << "10と同じです\n";
+  if(num >= 5 && num <= 10)
+    cout << "5と10の間にあります\n";
   return 0;
 }
