a0d290b035967cbfad356f848cbb45a8379325e9
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 12:27:25 +0900

    セクション6-6

--- sample.cpp
+++ sample.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int main (){
-  int num = 1;
-  int count = 0;
-  while(num < 1000){
-    num *= 2;
-    count++;
-    cout << count << "回目：" << num << "\n";
+  int num = 10;
+  do{
+    cout << "メッセージ\n";
+  }while(num < 5);
+  while(num < 5){
+    cout << "メッセージ\n";
   }
-  cout << count << "回目で1000を超えます\n";
   return 0;
 }
