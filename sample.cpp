1d4b83010fc563abb41fdc2013495edb8147770e
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 13:54:00 +0900

    セクション8-4

--- sample.cpp
+++ sample.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 
 int main (){
-  int array[3];
-  array[0] = 10;
-  array[1] = 20;
-  array[2] = 30;
+  int array[3] = {10, 20, 30};
   cout << array[0] << "\n";
   cout << array[1] << "\n";
   cout << array[2] << "\n";
