720c1154f4c6cefad50a2a605c88bcfa57272852
tamagokakepiza <nyororo89@gmail.com>
2022-07-16 11:58:34 +0900

    セクション5-9

--- sample.cpp
+++ sample.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int main (){
-  int age;
-  cout << "年齢を入力して下さい";
-  cin >> age;
-  if(age <= 3)
-    cout << "無料です\n";
-  else if(age <= 20)
-    cout << "1000円です\n";
-  else
-    cout << "2000円です\n";
+  int num;
+  cout << "番号を入力して下さい";
+  cin >> num;
+  switch(num){
+    case 1:
+      cout << "ハリー\n";
+      break;
+    case 2:
+      cout << "ロン\n";
+      break;
+    case 3:
+      cout << "ハーマイオニー\n";
+      break;
+    default:
+      cout << "マルフォイ\n";
+      break;
+  }
   return 0;
 }
